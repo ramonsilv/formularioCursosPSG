b6ad7cdd3c5893e49ae77d99e9557c81b01cb8a0
ramonsilv <ramonsilvaf@gmail.com>
2023-06-03 15:34:08 -0300

    modificação do formulario da pagina1 (FORMULARIO FUNCIONANDO COM JS) adção da pagina2.html-BOLETIMS
    Signed-off-by: ramonsilv <ramonsilvaf@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 function calc() {
-    let idade = document.getElementById('idade').value;
-    let renda = parseFloat(document.getElementById('renda').value);
-    let pessoas = parseInt(document.getElementById('pessoa').value);
+  let idade = document.getElementById("idade").value;
+  let renda = parseFloat(document.getElementById("renda").value);
+  let pessoas = parseInt(document.getElementById("pessoa").value);
 
-    const hoje = new Date();
-    const nasc = new Date(idade);
-    let idadeconver = hoje.getFullYear() - nasc.getFullYear();
+  const hoje = new Date();
+  const nasc = new Date(idade);
+  let idadeconver = hoje.getFullYear() - nasc.getFullYear();
 
-    rendapercapita = (renda / pessoas)
+  let rendapercapita = renda / pessoas;
+
+  if (idadeconver >= 16 && rendapercapita < 1500) {
+    document.getElementById("retorno").innerHTML =
+      "Seja Bem-Vindo,<br> Pode prosseguir!";
+  } else if (idadeconver < 16) {
+    document.getElementById("retorno").innerHTML =
+      "Não foi possivel realizar o cadadstro.<br> IDADE ABAIXO DE 16 ANOS.";
+  } else if (rendapercapita > 1500) {
+    document.getElementById("retorno").innerHTML =
+      "Não foi possivel realizar o cadastro.<br> RENDA ACIMA DE R$1500,00";
+  } else {
+    document.getElementById("retorno").innerHTML =
+      "Preencha os campos acima.";
+  }
 }
